e1e6544ae6870046a8aed7384324195d808ffde7
Peter Ojor <peter.ojor1@gmail.com>
2025-07-15 08:38:14 +0100

    Added more changes to the weather_advice.py file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,12 @@
-current_weather = input("What's the weather like today?(sunny/rainy/cold):").lower()
+current_weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 if current_weather == "sunny":
-	print("Wear a t-shirt and sunglasses.")
+        print("Wear a t-shirt and sunglasses.")
 elif current_weather == "rainy":
-	print("Don't forget your umbrella and a raincoat.")
+        print("Don't forget your umbrella and a raincoat.")
 elif current_weather == "cold":
-	print("Make sure to wear a warm coat and a staff.")
+        print("Make sure to wear a warm coat and a scarf.")
 else:
-	print("Sorry, i do not have recommendations for this weather.")
+        print("Sorry, i do not have recommendations for this weather.")
+
+
 
